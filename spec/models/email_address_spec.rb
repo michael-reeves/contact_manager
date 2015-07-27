07afcae9820eeb25e4d3ffc6825920be132c1310
Michael Reeves <mreeves68@yahoo.com>
2015-07-26 18:33:14 -0600

    Validate EmailAddress person presence

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
 
-  let(:email_address) { EmailAddress.new(address: 'alice@example.com') }
+  let(:email_address) {
+    EmailAddress.new(address: 'alice@example.com', person_id: 1)
+  }
 
   it 'is valid' do
     expect(email_address).to be_valid
@@ -10,6 +12,11 @@ RSpec.describe EmailAddress, type: :model do
 
   it 'is invalid without an address' do
     email_address.address = nil
+    expect(email_address).not_to be_valid
+  end
+
+  it 'must have a reference to a person' do
+    email_address.person_id = nil
     expect(email_address).not_to be_valid
   end
 

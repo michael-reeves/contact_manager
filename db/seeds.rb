80566988f8a525dd88f22d9a28dbcf304982330c
Michael Reeves <mreeves68@yahoo.com>
2015-07-26 19:25:38 -0600

    Display email addrs on Person#show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 person = Person.create(first_name: 'Alice', last_name: 'Smith')
 person.phone_numbers.create(number: '555-1234')
 person.phone_numbers.create(number: '555-9876')
+
+person.email_addresses.create(address: 'alice@yahoo.com')
+person.email_addresses.create(address: 'alice@gmail.com')
+person.email_addresses.create(address: 'alice@hotmail.com')
